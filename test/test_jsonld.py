8ed0fa2af985709a52d519acf425133414e2bece
Graham Higgins <gjh-github@bel-epa.com>
2012-02-08 12:16:20 +0000

    Pull in jsdonld tests from rdfextras core, refactor tox to use latest github dev versions of rdflib and rdfextras

--- test/test_jsonld.py
+++ test/test_jsonld.py
@@ -1,132 +1,81 @@
-import unittest
-# from nose.exc import SkipTest
+from glob import glob
+from os import path as p
+import json
 from rdflib import Graph
+from rdflib.compare import isomorphic
+# from rdfextras.ldcontext import Context
+from rdflib_jsonld.jsonld_parser import to_rdf
+from rdflib_jsonld.jsonld_serializer import to_tree
 
-test03_n3 = """\
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
 
-<http://example.org/people#joebob> a foaf:Person;
-  foaf:name "Joe Bob";
-  foaf:nick ( "joe" "bob" "jaybee" ) .
-"""
+test_dir = p.join(p.dirname(__file__), 'tests')
 
-test03_json = """\
-{
-  "@context":
-  {
-    "foaf": "http://xmlns.com/foaf/0.1/",
-    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
-  },
-  "@id": "http://example.org/people#joebob",
-  "@type": "foaf:Person",
-  "foaf:name": "Joe Bob",
-  "foaf:nick":
-  {
-    "@list": [ "joe", "bob", "jaybee" ]
-  }
-}
-"""
+def path_pairs():
+    for turtlepath in glob(p.join(test_dir, '*.ttl')):
+        commonpath = p.splitext(turtlepath)[0]
+        yield commonpath + '.jsonld', turtlepath
 
-test02_n3 = """\
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+def test_jsonld():
+    for jsonpath, turtlepath in path_pairs():
+        yield _test_serializer, jsonpath, turtlepath
+        yield _test_parser, jsonpath, turtlepath
 
-<http://manu.sporny.org/i/public>
-  a foaf:Person;
-  foaf:name "Manu Sporny";
-  foaf:knows [ a foaf:Person; foaf:name "Gregg Kellogg" ] .
-"""
+def _test_serializer(jsonpath, turtlepath):
+    test_tree, test_graph = _load_test_data(jsonpath, turtlepath)
 
-test02_json = """\
-{
-  "@context":
-  {
-    "foaf": "http://xmlns.com/foaf/0.1/",
-    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
-  },
-  "@id": "http://manu.sporny.org/i/public",
-  "@type": "foaf:Person",
-  "foaf:name": "Manu Sporny",
-  "foaf:knows":
-  {
-    "@type": "foaf:Person",
-    "foaf:name": "Gregg Kellogg"
-  }
-}
-"""
+    src_context = test_tree.get('@context')
+    result_tree = to_tree(test_graph, src_context, None, False)
 
-test01_n3 = """\
-@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+    expected = _to_json(_to_ordered(test_tree))
+    result = _to_json(_to_ordered(result_tree))
+    assert expected == result, \
+            "Expected JSON:\n%s\nGot:\n %s" % (expected, result)
 
-<http://manu.sporny.org/i/public> a foaf:Person;
-  foaf:name "Manu Sporny";
-  foaf:homepage <http://manu.sporny.org/> .
-"""
-test01_json = """\
-{
-  "@context":
-  {
-    "foaf": "http://xmlns.com/foaf/0.1/",
-    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
-  },
-  "@id": "http://manu.sporny.org/i/public",
-  "@type": "foaf:Person",
-  "foaf:name": "Manu Sporny",
-  "foaf:homepage": { "@id": "http://manu.sporny.org/" }
-}
-"""
+def _test_parser(jsonpath, turtlepath):
+    test_tree, test_graph = _load_test_data(jsonpath, turtlepath)
+    graph = to_rdf(test_tree, Graph())
+    assert isomorphic(graph, test_graph), \
+            "Expected graph:\n%s\nGot:\n %s" % (
+                    test_graph.serialize(format='n3'),
+                    graph.serialize(format='n3'))
 
-class JsonLDParserTestCase(unittest.TestCase):
-    identifier = "rdflib_test"
+# def _load_test_data(jsonpath, turtlepath):
+#     with open(jsonpath) as f:
+#         test_tree = json.load(f)
+#     test_graph = Graph().parse(turtlepath, format='n3')
+#     return test_tree, test_graph
 
-    def setUp(self):
-        self.json_graph = Graph()
-        self.n3_graph = Graph()
-    
-    def tearDown(self):
-        self.json_graph = self.n3_graph = None
+def _load_test_data(jsonpath, turtlepath):
+    f = open(jsonpath, 'r')
+    test_tree = json.load(f)
+    test_graph = Graph().parse(turtlepath, format='n3')
+    f.close()
+    return test_tree, test_graph
 
-    def test_parse01(self):
-        self.json_graph.parse(data=test01_json, format="json-ld")
-        self.n3_graph.parse(data=test01_n3, format="n3")
-        self.assertTrue(self.json_graph.isomorphic(self.n3_graph))
+#def _sparql_to_turtle(sparql):
+#    ttl = re.sub(r'(?i)PREFIX\s+([^>]+>)', r'@prefix \1 .', sparql)
+#    ttl = re.sub(r'(?si)ASK\s+WHERE\s*{(.*)}', r'\1', ttl)
+#    ttl = re.sub(r'\?(\w+)', r'_:\1', ttl)
+#    if not ttl.rstrip().endswith("."):
+#        ttl += " ."
+#    return ttl
 
-    def test_parse02(self):
-        self.json_graph.parse(data=test02_json, format="json-ld")
-        self.n3_graph.parse(data=test02_n3, format="n3")
-        self.assertTrue(self.json_graph.isomorphic(self.n3_graph))
+def _to_ordered(obj):
+    if not isinstance(obj, dict):
+        return obj
+    out = {}
+    for k, v in obj.items():
+        if isinstance(v, list):
+            # NOTE: use type in key to handle mixed lists of e.g. bool, int, float.
+            v = sorted((_to_ordered(lv) for lv in v),
+                    key=lambda x: (x, type(x).__name__))
+        else:
+            v = _to_ordered(v)
+        out[k] = v
+    return out
 
-    def test_parse03(self):
-        self.json_graph.parse(data=test03_json, format="json-ld")
-        self.n3_graph.parse(data=test03_n3, format="n3")
-        self.assertTrue(self.json_graph.isomorphic(self.n3_graph))
+def _to_json(tree):
+    return json.dumps(tree,
+            indent=4, separators=(',',': '),
+            sort_keys=True, check_circular=True)
 
-# Slightly lame, assumes JSON-LD parsing is error-free
-class JsonLDSerializerTestCase(unittest.TestCase):
-    identifier = "rdflib_test"
-
-    def setUp(self):
-        self.n3_graph = Graph()
-    
-    def tearDown(self):
-        self.n3_graph = None
-
-    def test_serialize_01(self):
-        self.n3_graph.parse(data=test01_n3, format="n3")
-        json_data = self.n3_graph.serialize(data=test01_json, format="json-ld")
-        gjson = Graph()
-        self.assert_(gjson.parse(data=json_data, format="json-ld").isomorphic(self.n3_graph))
-
-    def test_serialize_02(self):
-        self.n3_graph.parse(data=test02_n3, format="n3")
-        json_data = self.n3_graph.serialize(data=test02_json, format="json-ld")
-        gjson = Graph()
-        self.assert_(gjson.parse(data=json_data, format="json-ld").isomorphic(self.n3_graph))
-
-    def test_serialize_03(self):
-        self.n3_graph.parse(data=test03_n3, format="n3")
-        json_data = self.n3_graph.serialize(data=test03_json, format="json-ld")
-        gjson = Graph()
-        self.assert_(gjson.parse(data=json_data, format="json-ld").isomorphic(self.n3_graph))
