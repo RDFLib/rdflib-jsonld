62aef4f0dc689bb7573db331eb87fa61a9660544
cmchan27513 <Ming@192.168.10.1>
2012-05-26 23:29:30 -0400

    Parser needs to support "@graph" at the top level that is the new syntax... https://github.com/RDFLib/rdflib-jsonld/issues/5

--- rdflib_jsonld/jsonld_parser.py
+++ rdflib_jsonld/jsonld_parser.py
@@ -39,7 +39,7 @@ from rdflib.parser import Parser
 from rdflib.namespace import RDF, XSD
 from rdflib.term import URIRef, BNode, Literal
 
-from rdfextras.ldcontext import Context, Term, CONTEXT_KEY, ID_KEY, LIST_KEY
+from rdfextras.ldcontext import Context, Term, CONTEXT_KEY, ID_KEY, LIST_KEY, GRAPH_KEY
 from rdfextras.ldcontext import source_to_json
 
 __all__ = ['JsonLDParser', 'to_rdf']
@@ -106,6 +106,10 @@ def _add_to_graph(state, node):
         if pred_key == context.type_key:
             pred = RDF.type
             term = Term(None, None, context.id_key)
+        elif pred_key == context.graph_key:
+            for onode in obj_nodes:
+                _add_to_graph(state,onode)                        
+            continue
         else:
             pred_uri = context.expand(pred_key)
             pred = URIRef(pred_uri)
