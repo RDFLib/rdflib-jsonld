8aa0cd3199b69e3217ce285add388e293ee9d8f4
Graham Higgins <gjh-github@bel-epa.com>
2012-06-02 19:39:28 +0100

    I believe that we don't need this with the new setup entrypoints.

--- test/__init__.py
+++ test/__init__.py
@@ -4,11 +4,11 @@ from rdflib import parser
 import sys # sop to Hudson
 sys.path.insert(0, '/var/lib/tomcat6/webapps/hudson/jobs/rdfextras')
 
-plugin.register(
-        'json-ld', serializer.Serializer,
-        'rdflib_jsonld.jsonld_serializer', 'JsonLDSerializer')
+# plugin.register(
+#         'json-ld', serializer.Serializer,
+#         'rdflib_jsonld.jsonld_serializer', 'JsonLDSerializer')
 
-plugin.register(
-        'json-ld', parser.Parser,
-        'rdflib_jsonld.jsonld_parser', 'JsonLDParser')
+# plugin.register(
+#         'json-ld', parser.Parser,
+#         'rdflib_jsonld.jsonld_parser', 'JsonLDParser')
 
