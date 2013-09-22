848f77a5c5df091ef719a5a8b21782d71b3da57d
Marc-Antoine Parent <maparent@acm.org>
2013-09-22 18:51:54 -0400

    add test for direct parser access

--- test/test_testsuite.py
+++ test/test_testsuite.py
@@ -5,7 +5,7 @@ try:
     assert json
 except ImportError:
     import simplejson as json
-from rdflib import ConjunctiveGraph
+from rdflib import ConjunctiveGraph, Graph, Literal, URIRef
 from rdflib.compare import isomorphic
 from rdflib.py3compat import PY3
 from rdflib_jsonld.parser import to_rdf
@@ -84,6 +84,28 @@ def test_suite():
             func = _test_serializer
         #func.description = "%s-%s-%s" % (group, case)
         yield func, inputpath, expectedpath, context
+
+
+def test_parser():
+    test_json = '''
+    {
+        "@context": {
+            "dc": "http://purl.org/dc/terms/",
+            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+            "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
+        },
+        "@id": "http://example.org/about",
+        "dc:title": {
+            "@language": "en",
+            "@value": "Someone's Homepage"
+        }
+    }
+    '''
+    g = Graph().parse(data=test_json, format='json-ld')
+    assert list(g) == [(
+        URIRef('http://example.org/about'),
+        URIRef('http://purl.org/dc/terms/title'),
+        Literal("Someone's Homepage", lang='en'))]
 
 
 def _test_parser(inputpath, expectedpath, context):
