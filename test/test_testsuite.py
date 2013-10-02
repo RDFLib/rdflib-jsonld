f98e8c51b4d0ac9544415a6ca3e7ec80ae96a13a
Niklas Lindström <lindstream@gmail.com>
2013-10-02 22:40:33 +0200

    Move Graph.parse test to new test_api

--- test/test_testsuite.py
+++ test/test_testsuite.py
@@ -86,28 +86,6 @@ def test_suite():
         yield func, inputpath, expectedpath, context
 
 
-def test_parser():
-    test_json = '''
-    {
-        "@context": {
-            "dc": "http://purl.org/dc/terms/",
-            "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-            "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
-        },
-        "@id": "http://example.org/about",
-        "dc:title": {
-            "@language": "en",
-            "@value": "Someone's Homepage"
-        }
-    }
-    '''
-    g = Graph().parse(data=test_json, format='json-ld')
-    assert list(g) == [(
-        URIRef('http://example.org/about'),
-        URIRef('http://purl.org/dc/terms/title'),
-        Literal("Someone's Homepage", lang='en'))]
-
-
 def _test_parser(inputpath, expectedpath, context):
     input_obj = _load_json(inputpath)
     expected_graph = _load_nquads(expectedpath)
