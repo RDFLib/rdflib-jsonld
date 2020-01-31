7dfc7ebe3da4caeedfc0de003c4d477bacaaa310
Niklas Lindström <lindstream@gmail.com>
2020-01-31 23:37:23 +0100

    Move locally added tests to local-suite
    * Remove locally added tests from test/test-suite/ (copy of official
      test suite) and add to test/local-suite/
    * Factor out test suite runner (to test/runner.py)
    * Use runner in test/test_testsuite.py and test/test_localsuite.py

--- test/test_testsuite.py
+++ test/test_testsuite.py
@@ -1,33 +1,8 @@
-from __future__ import with_statement
 from os import environ, chdir, path as p
-try:
-    import json
-    assert json
-except ImportError:
-    import simplejson as json
+import json
 from rdflib import ConjunctiveGraph, Graph, Literal, URIRef
-from rdflib.compare import isomorphic
-from six import PY3
 import rdflib_jsonld.parser
-from rdflib_jsonld.parser import to_rdf
-from rdflib_jsonld.serializer import from_rdf
-from rdflib_jsonld.keys import CONTEXT, GRAPH
-
-
-rdflib_jsonld.parser.ALLOW_LISTS_OF_LISTS = False
-
-# monkey-patch NTriplesParser to keep source bnode id:s ..
-from rdflib.plugins.parsers.ntriples import NTriplesParser, r_nodeid, bNode
-def _preserving_nodeid(self):
-    if not self.peek('_'):
-        return False
-    return bNode(self.eat(r_nodeid).group(1))
-NTriplesParser.nodeid = _preserving_nodeid
-# .. and accept bnodes everywhere
-_uriref = NTriplesParser.uriref
-def _uriref_or_nodeid(self):
-    return _uriref(self) or self.nodeid()
-NTriplesParser.uriref = _uriref_or_nodeid
+from . import runner
 
 
 unsupported_tests = ("frame", "normalize")
@@ -47,8 +22,6 @@ import sys
 if sys.version_info[:2] < (2, 6):
     # Fails on bug in older urlparse.urljoin; ignoring..
     known_bugs += ('toRdf-0069-in','toRdf-0102-in')
-
-TC_BASE = "http://json-ld.org/test-suite/tests/"
 
 
 testsuite_dir = environ.get("JSONLD_TESTSUITE") or p.join(
@@ -82,6 +55,9 @@ def read_manifest(skiptests):
 
 
 def test_suite(skip_known_bugs=True):
+    default_allow = rdflib_jsonld.parser.ALLOW_LISTS_OF_LISTS
+    rdflib_jsonld.parser.ALLOW_LISTS_OF_LISTS = False
+
     skiptests = unsupported_tests
     if skip_known_bugs:
         skiptests += known_bugs
@@ -89,110 +65,16 @@ def test_suite(skip_known_bugs=True):
     for cat, num, inputpath, expectedpath, context, options in read_manifest(skiptests):
         if inputpath.endswith(".jsonld"): # toRdf
             if expectedpath.endswith(".jsonld"): # compact/expand/flatten
-                func = _test_json
+                func = runner.do_test_json
             else: # toRdf
-                func = _test_parser
+                func = runner.do_test_parser
         else:  # fromRdf
-            func = _test_serializer
+            func = runner.do_test_serializer
         #func.description = "%s-%s-%s" % (group, case)
         yield func, cat, num, inputpath, expectedpath, context, options
 
+    rdflib_jsonld.parser.ALLOW_LISTS_OF_LISTS = default_allow
 
-def _test_json(cat, num, inputpath, expectedpath, context, options):
-    base = TC_BASE + inputpath
-    input_obj = _load_json(inputpath)
-    input_graph = ConjunctiveGraph()
-    to_rdf(input_obj, input_graph, base=base, context_data=context,
-            produce_generalized_rdf=True)
-    expected_json = _load_json(expectedpath)
-    use_native_types = True # CONTEXT in input_obj
-    result_json = from_rdf(input_graph, context, base=TC_BASE + inputpath,
-            use_native_types=options.get('useNativeTypes', use_native_types),
-            use_rdf_type=options.get('useRdfType', False))
-
-    def _prune_json(data):
-        if CONTEXT in data:
-            data.pop(CONTEXT)
-        if GRAPH in data:
-            data = data[GRAPH]
-        #def _remove_empty_sets(obj):
-        return data
-
-    expected_json = _prune_json(expected_json)
-    result_json = _prune_json(result_json)
-
-    _compare_json(expected_json, result_json)
-
-
-def _test_parser(cat, num, inputpath, expectedpath, context, options):
-    input_obj = _load_json(inputpath)
-    expected_graph = _load_nquads(expectedpath)
-    base = TC_BASE + inputpath
-    result_graph = ConjunctiveGraph()
-    to_rdf(input_obj, result_graph, base=base, context_data=context,
-            produce_generalized_rdf = options.get('produceGeneralizedRdf', False))
-    assert isomorphic(
-            result_graph, expected_graph), "Expected:\n%s\nGot:\n%s" % (
-            expected_graph.serialize(format='turtle'),
-            result_graph.serialize(format='turtle'))
-
-
-def _test_serializer(cat, num, inputpath, expectedpath, context, options):
-    input_graph = _load_nquads(inputpath)
-    expected_json = _load_json(expectedpath)
-    result_json = from_rdf(input_graph, context, base=TC_BASE + inputpath,
-            use_native_types=options.get('useNativeTypes', False),
-            use_rdf_type=options.get('useRdfType', False))
-    _compare_json(expected_json, result_json)
-
-
-def _load_nquads(source):
-    graph = ConjunctiveGraph()
-    with open(source) as f:
-        if PY3:
-            data = f.read()
-        else:
-            data = f.read().decode('utf-8')
-    graph.parse(data=data, format='nquads')
-    return graph
-
-
-def _load_json(source):
-    with open(source) as f:
-        return json.load(f)
-
-
-def _to_ordered(obj):
-    if isinstance(obj, list):
-        # NOTE: use type in key to handle mixed
-        # lists of e.g. bool, int, float.
-        return sorted((_to_ordered(lv) for lv in obj),
-                key=lambda x: (_ord_key(x), type(x).__name__))
-    if not isinstance(obj, dict):
-        return obj
-    return sorted((k, _to_ordered(v))
-            for k, v in obj.items())
-
-
-def _ord_key(x):
-    if isinstance(x, dict) and '@id' in x:
-        return x['@id']
-    else:
-        return x
-
-
-def _dump_json(obj):
-    return json.dumps(obj,
-            indent=4, separators=(',', ': '),
-            sort_keys=True, check_circular=True)
-
-
-def _compare_json(expected, result):
-    expected = json.loads(_dump_json(expected))
-    result = json.loads(_dump_json(result))
-    assert _to_ordered(expected) == _to_ordered(result), \
-            "Expected JSON:\n%s\nGot:\n%s" % (
-                    _dump_json(expected), _dump_json(result))
 
 if __name__ == '__main__':
     import sys
