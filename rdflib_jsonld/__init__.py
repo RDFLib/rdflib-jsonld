483547fe557bad472b55b2bc5955f5d77744d00e
Alex Nelson <alexander.nelson@nist.gov>
2021-09-10 17:22:56 -0400

    Add deprecation warning on top-level import
    The message provided in this patch is a first draft and invited for
    revision by a project maintainer.
    
    The context manager is to contain the effect of
    `warnings.simplefilter()`, prevent an import of `rdflib-jsonld` from
    introducing unexpected behaviors from other applications using the
    `warnings` module.
    
    References:
    * https://github.com/RDFLib/rdflib/issues/1405
    * https://docs.python.org/3/library/warnings.html#temporarily-suppressing-warnings
    
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- rdflib_jsonld/__init__.py
+++ rdflib_jsonld/__init__.py
@@ -1,3 +1,9 @@
 """
 """
 __version__ = "0.6.0-dev"
+
+import warnings
+
+with warnings.catch_warnings():
+    warnings.simplefilter("default")
+    warnings.warn("The rdflib-jsonld package has been integrated into rdflib as of rdflib==6.0.0.  Please remove rdflib-jsonld from your project's dependencies.", DeprecationWarning)
